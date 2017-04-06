6f88072a1c050f5bf6c52a0b54523b10caaf477a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2017-04-06 23:38:37 +0100

    made more PWA changes

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,5 @@
 const title = 'viperHTML 🐍 Hacker News';
+const viperHTML = require('viperhtml');
 const render = require('./render');
 
 // basically this is the router
@@ -104,3 +105,23 @@ addEventListener('popstate', showPage);
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 }
+
+// add PWA related stuff
+this.onload = () => {
+  document.head.appendChild(
+    viperHTML.wire()`
+      <link rel="apple-touch-icon" sizes="57x57" href="/img/apple-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="/img/apple-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="/img/apple-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="/img/apple-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="/img/apple-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="/img/apple-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="/img/apple-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon-180x180.png">
+      <link rel="icon" type="image/png" sizes="192x192"  href="/img/android-icon-192x192.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    `
+  );
+};
